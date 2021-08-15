251c061d6b956df795234db076f5f995891b63b2
Sub-Zerro <85824405+Sub-Zerro@users.noreply.github.com>
2021-08-15 12:40:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 'use strict';
 
 let enter;
-const button = document.querySelector('.btn');
-let img = document.createElement("img");
-img.src = "https://cdn.discordapp.com/attachments/876035186305941524/876127281033138216/dac3bc690788712b.png";
+const btn = document.querySelector('.btn');
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+
+
+
+
+
+let a;
+let img1 = document.createElement("img");
+img1.src = "https://cdn.discordapp.com/attachments/876035186305941524/876127281033138216/dac3bc690788712b.png";
+img1.width = "800";
 let div = document.createElement('div');
+
+
+
+
 
 
 function parol () {
@@ -22,10 +36,25 @@ while (enter!="Огурец226") {
         console.log('Сайт работает'); // Проверочный вывод для понятия подключения сайта к html коду
 
         
-        button.addEventListener('click', () => {
-            div.append(img);
-            document.body.append(div);
+        btn.addEventListener('click', () => {
+        div.append(img1);
+        document.body.append(div);
         });
+
+        btn1.addEventListener('click', () => {
+            alert("Все ок");
+
+        });
+
+        btn2.addEventListener('click', () => {
+            alert("Это 3 кнопка");
+        });
+
+        btn3.addEventListener('dblclick', () => {
+            window.location.replace('https://www.youtube.com/channel/UC45_tmUzeXEV4UoN1XMY44A');
+        });
+
+
 
     
 
@@ -41,8 +70,6 @@ while (enter!="Огурец226") {
 
     };    
 };
-
-
 
 
 
